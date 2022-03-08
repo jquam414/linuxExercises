241a0998290d95dc52e3ca95cfeae601107640d2
Connor Weida <cweida@wisc.edu>
2022-03-08 17:37:41 -0600

    connor add 1

--- mean.sh
+++ mean.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+echo "usage: mean.sh <column> [file.csv]"
+column=$1
+file=$2
+echo "file=$file, column=$column"
+
+# Then I want to use a pipeline
+
+
+
+#while read line; do
+ #   echo "line"
+  #  break
+#done < file
+
+
