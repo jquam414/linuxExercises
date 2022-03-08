88c9648779660061ad23fd35b2aac497065c79f4
John Quam <jdquam@wisc.edu>
2022-03-08 15:12:27 -0600

    Wrote the script to output the average

--- school.sh
+++ school.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+#replace head -n 5 Property_Tax_Roll.csv  with cat Property_Tax_Roll.csv
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | { sum=0; n=0; while read val; do sum=$(($sum + $val)); n=$(($n +1)); done; echo $sum/$n; } | bc -l
