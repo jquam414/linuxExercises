35cb232684d7e20b61584ad77b813c203c1a270d
Jack Kiiskila <jwkiiskila@wisc.edu>
2022-03-10 06:39:15 -0600

    Jacks first commit

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#usage statement wrote to stderr
+echo "usage: $0 <dir> <n>" 1>&2
+
+#stores parameters
+
+dir=$1
+n=$2
+
+#finds files within the directory provided that are larger than n bytes are removes them 
+
+file2=$(find ./$dir -type f -size +${n}c -delete)
+
+
+
